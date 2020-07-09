4eedfffd76c651fb7948ab85e8a7ab84dfada3ca
Aaron Edmonds <Aaron.Edmonds@outlook.com>
2020-07-08 22:19:35 -0700

    moved the task away from main into its own method

--- Program.cs
+++ Program.cs
@@ -13,46 +13,48 @@ namespace TimerFun
 
         static async Task Main(string[] args)
         {
-            DateTime dt = DateTime.Now;
-            TimeSpan ts = new TimeSpan(23,10, 0); // 10:20pm
-            dt = dt.Date + ts; // set date time to same date, but time ahead
+            await PerformActions();
 
-            Action doWorkAction = new Action(DoWork);  // call dowork method.
-            // doWorkAction(); // print Hi, I am doing work (calls the dowork method
-            Console.WriteLine("Hi");
-            await ScheduleAction(doWorkAction, dt);
-            //Execute(doWorkAction, dt);
-            Console.WriteLine("Timer set for {0} now off to do cool things while we wait!", ts);
-            for (int i=0;i<10000;i++)
-            {
-                Console.Write("{0} ",i);
-                Thread.Sleep(1000);
-            }
-            
 
         }
 
-        public static void DoWork()
+        public static async Task PerformActions()
+        {
+        DateTime dt = DateTime.Now;
+        TimeSpan ts = new TimeSpan(22, 08, 0); // 10:20pm
+        dt = dt.Date + ts; // set date time to same date, but time ahead
+
+            Action doWorkAction = new Action(DoWork);  // call dowork method.
+                                                       // doWorkAction(); // print Hi, I am doing work (calls the dowork method
+        Console.WriteLine("Hi");
+            await ScheduleAction(doWorkAction, dt);  // was awaited
+                                                     //Execute(doWorkAction, dt);
+        Console.WriteLine("Timer set for {0} now off to do cool things while we wait!", ts);
+            for (int i = 0; i< 1000; i++)
+            {
+                Console.Write("{0} ", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+
+
+
+
+public static void DoWork()
         {
             Console.WriteLine("Hi, I am doing work!");
 
         }
-        public static void Execute(Action action, DateTime ExecutionTime) // so it looks like the Action here is the method to call...
-        {
-            double mathstuff = (int)ExecutionTime.Subtract(DateTime.Now).TotalMilliseconds;
-            Console.WriteLine("mathstuff = MS {0}", mathstuff);
-
-            Task WaitTask = Task.Delay((int)mathstuff).ContinueWith(t => action);
-            //Task WaitTask = Task.Delay((int)DateTime.Now.Subtract(ExecutionTime).TotalMilliseconds);
-            //WaitTask.ContinueWith(t => action);
-            //WaitTask.Start();
-        }
 
         // using await
-        public async static Task ScheduleAction(Action action, DateTime ExecutionTime)
+        public static async Task ScheduleAction(Action action, DateTime ExecutionTime)
         {
-            await Task.Delay((int)ExecutionTime.Subtract(DateTime.Now).TotalMilliseconds);
+             await Task.Delay((int)ExecutionTime.Subtract(DateTime.Now).TotalMilliseconds);
             action();
+            
+            
+            
             
         }
 
